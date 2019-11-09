3711ccf7f4c0ccfe8516002ca87bec9fab5c2395
AIRTucha <airtucha@icloud.com>
2019-11-09 13:47:43 +0100

    Tests for the lib

---
+++ tests/index.ts
@@ -0,0 +1,82 @@
+import { expect } from 'chai'
+import { getExtension, getContentType, mbGetContentType as getContentTypeMaybe } from '../src/index'
+import { Just, None } from 'amonad'
+
+describe("getExtension", () => {
+  it("simple name", () => {
+    expect(getExtension("test")).eql(undefined)
+  })
+
+  it("simple name with extension", () => {
+    expect(getExtension("test.txt")).eql("txt")
+  })
+
+  it("complex absolute path", () => {
+    expect(getExtension("text1/test")).eql(undefined)
+  })
+
+  it("complex absolute path with extension", () => {
+    expect(getExtension("text1/test.jpg")).eql("jpg")
+  })
+
+  it("complex relative path", () => {
+    expect(getExtension("./text1/test")).eql(undefined)
+  })
+
+  it("complex relative path with extension", () => {
+    expect(getExtension("./text1/test.mpeg")).eql("mpeg")
+  })
+})
+
+describe("getContentType", () => {
+  it("simple name", () => {
+    expect(getContentType("test")).eql(undefined)
+  })
+
+  it("simple name with extension", () => {
+    expect(getContentType("test.txt")).eql("text/plain")
+  })
+
+  it("complex absolute path", () => {
+    expect(getContentType("text1/test")).eql(undefined)
+  })
+
+  it("complex absolute path with extension", () => {
+    expect(getContentType("text1/test.jpg")).eql("image/jpeg")
+  })
+
+  it("complex relative path", () => {
+    expect(getContentType("./text1/test")).eql(undefined)
+  })
+
+  it("complex relative path with extension", () => {
+    expect(getContentType("./text1/test.mpeg")).eql("video/mpeg")
+  })
+})
+
+
+describe("getContentTypeMaybe", () => {
+  it("simple name", () => {
+    expect(getContentTypeMaybe("test")).eql(None())
+  })
+
+  it("simple name with extension", () => {
+    expect(getContentTypeMaybe("test.txt")).eql(Just("text/plain"))
+  })
+
+  it("complex absolute path", () => {
+    expect(getContentTypeMaybe("text1/test")).eql(None())
+  })
+
+  it("complex absolute path with extension", () => {
+    expect(getContentTypeMaybe("text1/test.jpg")).eql(Just("image/jpeg"))
+  })
+
+  it("complex relative path", () => {
+    expect(getContentTypeMaybe("./text1/test")).eql(None())
+  })
+
+  it("complex relative path with extension", () => {
+    expect(getContentTypeMaybe("./text1/test.mpeg")).eql(Just("video/mpeg"))
+  })
+})
